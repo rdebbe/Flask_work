a31a109dfbabe258ba7fc9452a83fbdf1de75d26
Ramiro Debbe <ramirodebbe@gmail.com>
2017-01-22 21:35:39 -0500

    working on forms and validation

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import Form 
+from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
+class SignupForm(FlaskForm):
   first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
   last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
   email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
